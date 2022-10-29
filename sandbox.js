e6fb84ebb60dfa05d760e8b0195d9df07a50dea6
Mochamad Alfin Rakhmat Fadillah <114910587+alfinfadillah@users.noreply.github.com>
2022-10-29 07:12:19 +0700

    Update sandbox.js
    Menambahkan jawaban dari soal pertama hingga soal kelima (sorry baru ngirim sekarang, karena gw baru tau kalo ngisinya disini dan baru ngerti github, sebenarnya gw udah kelar sehabis dari rumah lu, cuma gw ngerjainnya di vscode)

--- sandbox.js
+++ sandbox.js
@@ -7,19 +7,19 @@ var angkaKetiga = 5
 var angkaKeempat = 7
 
 // 1. Tulis kode diline setelah comment ini
-
+angkaPertama = angkaKetiga + angkaKedua - angkaPertama
 console.log(angkaPertama) // 7
 
 // 2. Tulis kode diline setelah comment ini
-
+angkaKedua = angkaKedua - angkaKetiga + angkaPertama -angkaKetiga
 console.log(angkaKedua) // 1
 
 // 3. Tulis kode diline setelah comment ini
-
+angkaKeempat = angkaPertama + angkaKeempat
 console.log(angkaKeempat) // 14
 
 // 4. Tulis kode diline setelah comment ini
-
+angkaKetiga = angkaKetiga * angkaKeempat
 console.log(angkaKetiga) // 70
 
 // ====================================================
@@ -32,4 +32,5 @@ var kalimatKedua = 'namaku'
 var kalimatKetiga = 'Alfin'
 var kalimatKeempat = ', aku sedang belajar coding'
 
+hasil = kalimatPertama + " " + kalimatKedua + " " + kalimatKetiga + kalimatKeempat
 console.log(hasil) // Halo namaku Alfin, aku sedang belajar coding 
